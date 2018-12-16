02ce7c6c0182f62d1d5e4c151760aa8490becbdf
Yong Jin Lee <cyberbrian@naver.com>
2018-12-16 18:47:16 +0900

    Add codes for router

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"blog/router"
 	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello", r.URL.Path[1:])
-}
-
 func main() {
 
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	mux := router.InitRouter()
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
 
 }
---
+++ router/router.go
@@ -0,0 +1,64 @@
+package router
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Route struct {
+	Name    string
+	Address string
+}
+
+var Routes = map[Route]http.HandlerFunc{
+	Route{"IndexPage", "/"}:             IndexHandler,
+	Route{"BlogList", "/blog"}:          BlogListHandler,
+	Route{"BlogPage", "/blog/:"}:        BlogPageHandler,
+	Route{"ProjectList", "/projects"}:   ProjectListHandler,
+	Route{"ProjectPage", "/projects/:"}: ProjectPageHandler,
+	Route{"VisitingNotes", "/visiting"}: VisitingNotesHandler,
+	Route{"Portfolio", "/Portfolio"}:    PortfolioHandler,
+}
+
+func InitRouter() *http.ServeMux {
+	m := http.NewServeMux()
+
+	for rt, h := range Routes {
+		m.Handle(rt.Address, h)
+	}
+	return m
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "HI", r.URL.Path[1:])
+}
+func BlogListHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello", r.URL.Path[1:])
+}
+func BlogPageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the index page", r.URL.Path[1:])
+}
+func ProjectListHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the index page", r.URL.Path[1:])
+}
+func ProjectPageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the index page", r.URL.Path[1:])
+}
+
+func VisitingNotesHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the index page", r.URL.Path[1:])
+}
+
+func PortfolioHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the index page", r.URL.Path[1:])
+}
+
+//Router needs
+/*
+* main page -> / -> show list of blog posts
+* blogs -> blogs
+* Intro -> introduction
+* projects -> projects
+* visitor page -> visiting
+* Portfolio -> portfolio
+ */
