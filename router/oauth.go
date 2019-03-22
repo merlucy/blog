dbfb4138727b5df3aa98d096a4577ff482f7a12c
Yong Jin Lee <cyberbrian@naver.com>
2019-03-22 11:33:52 +0900

    Add Google login

--- router/oauth.go
+++ router/oauth.go
@@ -22,36 +22,20 @@ var (
 	oauthStateString = "jeffkim"
 )
 
-const htmlIndex = `<html><body>
-Logged in with <a href="/login">Google</a>
-</body></html>
-`
-
 func GoogleLoginHandler(w http.ResponseWriter, r *http.Request) {
 
 	url := oauthConf.AuthCodeURL(oauthStateString)
 
-	/*
-		Url, err := url.Parse(oauthConf.Endpoint.AuthURL)
-		if err != nil {
-			log.Fatal("Parse: ", err)
-		}
-
-		parameters := url.Values{}
-		parameters.Add("client_id", oauthConf.ClientID)
-		parameters.Add("scope", strings.Join(oauthConf.Scopes, " "))
-		parameters.Add("redirect_uri", oauthConf.RedirectURL)
-		parameters.Add("response_type", "code")
-		parameters.Add("state", oauthStateString)
-		Url.RawQuery = parameters.Encode()
-		url := Url.String()
-	*/
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+
 }
 
 func GoogleCallbackHandler(w http.ResponseWriter, r *http.Request) {
+
 	state := r.FormValue("state")
+
 	if state != oauthStateString {
+
 		fmt.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
@@ -60,6 +44,7 @@ func GoogleCallbackHandler(w http.ResponseWriter, r *http.Request) {
 	code := r.FormValue("code")
 
 	token, err := oauthConf.Exchange(oauth2.NoContext, code)
+
 	if err != nil {
 		fmt.Printf("oauthConf.Exchange() failed with '%s'\n", err)
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
@@ -68,14 +53,17 @@ func GoogleCallbackHandler(w http.ResponseWriter, r *http.Request) {
 
 	resp, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" +
 		url.QueryEscape(token.AccessToken))
+
 	if err != nil {
 		fmt.Printf("Get: %s\n", err)
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
+
 	defer resp.Body.Close()
 
 	response, err := ioutil.ReadAll(resp.Body)
+
 	if err != nil {
 		fmt.Printf("ReadAll: %s\n", err)
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
